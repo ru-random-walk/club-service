0c6f7aea1d0d8a1a07ae6e2ede0d2c1461cbe4bc
maksmolchdmitr <maksmolchdmitr@gmail.com>
2024-12-22 08:11:46 +0300

    RANDOMWALK-20: remove unused query

--- src/main/java/ru/random/walk/club_service/controller/ClubController.java
+++ src/main/java/ru/random/walk/club_service/controller/ClubController.java
@@ -7,32 +7,13 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import ru.random.walk.club_service.model.entity.ClubEntity;
-import ru.random.walk.club_service.model.entity.MemberEntity;
-import ru.random.walk.club_service.model.graphql.types.PaginationInput;
 import ru.random.walk.club_service.util.StubDataUtil;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.UUID;
 
 @Controller
 @Slf4j
 public class ClubController {
-    @QueryMapping
-    List<MemberEntity> getClubMembers(
-            @Argument UUID clubId,
-            @Argument PaginationInput pagination
-    ) {
-        log.info("""
-                        Get club members
-                        by club id [{}]
-                        with pagination [{}]
-                        """,
-                clubId, pagination
-        );
-        return Collections.singletonList(StubDataUtil.memberEntity());
-    }
-
     @QueryMapping
     @Nullable ClubEntity getClub(@Argument UUID clubId) {
         log.info("""
