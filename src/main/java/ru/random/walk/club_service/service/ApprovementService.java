11bb35d54b9d14867356370b9d0c308435f7281b
maksmolchdmitr <maksmolchdmitr@gmail.com>
2025-02-10 00:19:27 +0300

    RANDOMWALK-112: rewrite services with interface

--- src/main/java/ru/random/walk/club_service/service/ApprovementService.java
+++ src/main/java/ru/random/walk/club_service/service/ApprovementService.java
@@ -1,67 +1,19 @@
 package ru.random.walk.club_service.service;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
 import ru.random.walk.club_service.model.domain.approvement.FormApprovementData;
 import ru.random.walk.club_service.model.domain.approvement.MembersConfirmApprovementData;
 import ru.random.walk.club_service.model.entity.ApprovementEntity;
-import ru.random.walk.club_service.model.entity.type.ApprovementType;
-import ru.random.walk.club_service.model.exception.NotFoundException;
-import ru.random.walk.club_service.repository.ApprovementRepository;
-import ru.random.walk.club_service.repository.ClubRepository;
 
 import java.util.UUID;
 
-@Service
-@AllArgsConstructor
-public class ApprovementService {
-    private final ApprovementRepository approvementRepository;
-    private final ClubRepository clubRepository;
+public interface ApprovementService {
+    ApprovementEntity addForClub(MembersConfirmApprovementData membersConfirmData, UUID clubId);
 
-    public ApprovementEntity addForClub(MembersConfirmApprovementData membersConfirmData, UUID clubId) {
-        var club = clubRepository.findById(clubId)
-                .orElseThrow(() -> new NotFoundException("Club with such id not found!"));
-        var approvement = ApprovementEntity.builder()
-                .club(club)
-                .clubId(clubId)
-                .data(membersConfirmData)
-                .type(ApprovementType.MEMBERS_CONFIRM)
-                .build();
-        return approvementRepository.save(approvement);
-    }
+    ApprovementEntity addForClub(FormApprovementData formApprovementData, UUID clubId);
 
-    public ApprovementEntity addForClub(FormApprovementData formApprovementData, UUID clubId) {
-        var club = clubRepository.findById(clubId)
-                .orElseThrow(() -> new NotFoundException("Club with such id not found!"));
-        var approvement = ApprovementEntity.builder()
-                .club(club)
-                .clubId(clubId)
-                .data(formApprovementData)
-                .type(ApprovementType.FORM)
-                .build();
-        return approvementRepository.save(approvement);
-    }
+    ApprovementEntity update(MembersConfirmApprovementData membersConfirmData, UUID approvementId);
 
-    public ApprovementEntity update(MembersConfirmApprovementData membersConfirmData, UUID approvementId) {
-        var approvement = approvementRepository.findById(approvementId)
-                .orElseThrow(() -> new NotFoundException("Approvement with such id not found!"));
-        approvement.setData(membersConfirmData);
-        approvement.setType(ApprovementType.MEMBERS_CONFIRM);
-        return approvementRepository.save(approvement);
-    }
+    ApprovementEntity update(FormApprovementData formApprovementData, UUID approvementId);
 
-    public ApprovementEntity update(FormApprovementData formApprovementData, UUID approvementId) {
-        var approvement = approvementRepository.findById(approvementId)
-                .orElseThrow(() -> new NotFoundException("Approvement with such id not found!"));
-        approvement.setData(formApprovementData);
-        approvement.setType(ApprovementType.FORM);
-        return approvementRepository.save(approvement);
-    }
-
-    public UUID delete(UUID approvementId) {
-        var approvement = approvementRepository.findById(approvementId)
-                .orElseThrow(() -> new NotFoundException("Approvement with such id not found!"));
-        approvementRepository.delete(approvement);
-        return approvement.getId();
-    }
+    UUID delete(UUID approvementId);
 }
--- src/main/java/ru/random/walk/club_service/service/Authenticator.java
+++ src/main/java/ru/random/walk/club_service/service/Authenticator.java
@@ -1,34 +1,10 @@
 package ru.random.walk.club_service.service;
 
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Service;
-import ru.random.walk.club_service.model.entity.type.MemberRole;
-import ru.random.walk.club_service.model.exception.AuthenticationException;
-import ru.random.walk.club_service.model.exception.NotFoundException;
-import ru.random.walk.club_service.repository.ApprovementRepository;
-import ru.random.walk.club_service.repository.MemberRepository;
-
 import java.security.Principal;
 import java.util.UUID;
 
-@Service
-@AllArgsConstructor
-public class Authenticator {
-    private final MemberRepository memberRepository;
-    private final ApprovementRepository approvementRepository;
+public interface Authenticator {
+    void authAdminByClubId(Principal principal, UUID clubId);
 
-    public void authAdminByClubId(Principal principal, UUID clubId) {
-        var login = UUID.fromString(principal.getName());
-        var member = memberRepository.findByIdAndClubId(login, clubId)
-                .orElseThrow(() -> new AuthenticationException("You are not become member of given club!"));
-        if (member.getRole() != MemberRole.ADMIN) {
-            throw new AuthenticationException("You are not authorized to access this club members!");
-        }
-    }
-
-    public void authAdminByApprovementId(Principal principal, UUID approvementId) {
-        var approvement = approvementRepository.findById(approvementId)
-                .orElseThrow(() -> new NotFoundException("Approvement with such id not found!"));
-        authAdminByClubId(principal, approvement.getClubId());
-    }
+    void authAdminByApprovementId(Principal principal, UUID approvementId);
 }
