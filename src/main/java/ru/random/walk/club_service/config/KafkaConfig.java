4ec86f7d74ee5d4b8bbd7cb57b16e2676c5a4cf5
maksmolchdmitr <maksmolchdmitr@gmail.com>
2025-03-02 14:28:13 +0300

    RANDOMWALK-128: refactor kafka config

--- src/main/java/ru/random/walk/club_service/config/KafkaConfig.java
+++ src/main/java/ru/random/walk/club_service/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package ru.random.walk.club_service.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +11,8 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+import ru.random.walk.dto.RegisteredUserInfoEvent;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,23 +21,35 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConfig {
     @Bean
-    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory(KafkaProperties kafkaProperties) {
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerConfig(kafkaProperties));
+    public KafkaListenerContainerFactory<?> registeredUserInfoEventKafkaListenerContainerFactory(
+            KafkaProperties kafkaProperties
+    ) {
+        ConcurrentKafkaListenerContainerFactory<String, RegisteredUserInfoEvent> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(
+                consumerFactoryWithStringKey(kafkaProperties, getRegisteredUserInfoEventJsonDeserializer())
+        );
         return factory;
     }
 
-    public ConsumerFactory<String, String> consumerConfig(KafkaProperties kafkaConfig) {
+    public <T> ConsumerFactory<String, T> consumerFactoryWithStringKey(
+            KafkaProperties kafkaProperties,
+            JsonDeserializer<T> deserializer
+    ) {
         Map<String, Object> properties = new HashMap<>();
-
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaConfig.getBootstrapServers());
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConfig.getConsumer().getGroupId());
-        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        properties.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 10);
-        properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaProperties.getConsumer().getGroupId());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer);
+        return new DefaultKafkaConsumerFactory<>(properties, new StringDeserializer(), deserializer);
+    }
 
-        return new DefaultKafkaConsumerFactory<>(properties);
+    @NotNull
+    private static JsonDeserializer<RegisteredUserInfoEvent> getRegisteredUserInfoEventJsonDeserializer() {
+        var deserializer = new JsonDeserializer<>(RegisteredUserInfoEvent.class);
+        deserializer.addTrustedPackages("*");
+        deserializer.setRemoveTypeHeaders(false);
+        deserializer.setUseTypeMapperForKey(true);
+        return deserializer;
     }
 }
