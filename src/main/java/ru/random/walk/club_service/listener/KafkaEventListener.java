baaf4b3b640e93a0d587dff16ca22c6e38630e35
maksmolchdmitr <maksmolchdmitr@gmail.com>
2025-03-03 11:58:05 +0300

    RANDOMWALK-128: separate service logic from listener

--- src/main/java/ru/random/walk/club_service/listener/KafkaEventListener.java
+++ src/main/java/ru/random/walk/club_service/listener/KafkaEventListener.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 import ru.random.walk.club_service.mapper.UserMapper;
-import ru.random.walk.club_service.repository.UserRepository;
+import ru.random.walk.club_service.service.UserService;
 import ru.random.walk.dto.RegisteredUserInfoEvent;
 import ru.random.walk.topic.EventTopic;
 
@@ -13,13 +13,13 @@ import ru.random.walk.topic.EventTopic;
 @Slf4j
 @AllArgsConstructor
 public class KafkaEventListener {
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final UserMapper userMapper;
 
     @KafkaListener(topics = EventTopic.USER_REGISTRATION, containerFactory = "registeredUserInfoEventKafkaListenerContainerFactory")
     public void listenRegisteredUserInfoEvent(RegisteredUserInfoEvent event) {
         log.info("User was registered with event info: {}", event);
         var userEntity = userMapper.toEntity(event);
-        userRepository.save(userEntity);
+        userService.add(userEntity);
     }
 }
---
+++ src/main/java/ru/random/walk/club_service/service/UserService.java
@@ -0,0 +1,7 @@
+package ru.random.walk.club_service.service;
+
+import ru.random.walk.club_service.model.entity.UserEntity;
+
+public interface UserService {
+    void add(UserEntity userEntity);
+}
